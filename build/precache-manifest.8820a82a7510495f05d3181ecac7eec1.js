9d71b6ce749819696278bac77c781b4de9b2594f
obamo1 <joobamo@gmail.com>
2022-04-28 17:00:27 +0100

    ini

---
+++ build/precache-manifest.8820a82a7510495f05d3181ecac7eec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701c99b0d44f60b8207ba675abc8aaa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a167491cc7a1d7f117ec",
+    "url": "/static/css/main.5ca41be2.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "a167491cc7a1d7f117ec",
+    "url": "/static/js/main.c27c475e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import MultipleReturns from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
-// import ShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit';
+import ShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit';
 import ShowHide from './tutorial/3-conditional-rendering/setup/3-show-hide';
  
 ReactDOM.render(
-    <ShowHide />,
+    <ShortCircuit/>,
   
   document.getElementById('root')
 );
