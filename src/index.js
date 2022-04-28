93864be895a34039381eba6217ba2c2656a510ce
obamo1 <joobamo@gmail.com>
2022-04-28 17:15:16 +0100

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import MultipleReturns from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import MultipleReturns from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
 import ShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit';
 import ShowHide from './tutorial/3-conditional-rendering/setup/3-show-hide';
  
